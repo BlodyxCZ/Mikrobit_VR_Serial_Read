114987ad1bc180ac11852ec269849f238098fac0
BlodyxCZ <kjubercz@gmail.com>
2023-09-26 11:03:20 +0200

    Updated main script

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 ser = serial.Serial('COM6', 115200)
 d_path = "C:\\Users\\kjube\\AppData\\Roaming\\Godot\\app_userdata\\VR Space Driver\\"
-device = "Gun"
+device = ""
 
 
 def strtobool(val):
@@ -24,24 +24,24 @@ while True:
         data = str(data)
         data = data.lstrip("b'")
         data = data.rstrip("'")
+        json_data = {}
         if data[:1] == "G":
             device = "Gun"
             data = data.lstrip("G")
             data = data.split(",")
             if len(data) != 11:
                 break
-            json_data = {"raw": data[0], "pitch": data[1], "rotation": data[2], "vel_x": data[3],
-                         "vel_z": data[4], "A": strtobool(data[5]), "B": strtobool(data[6]),
-                         "C": strtobool(data[7]), "D": strtobool(data[8]), "E": strtobool(data[9]),
-                         "F": strtobool(data[10]), "device": device}
-        else:
+            json_data = {"raw": data[0], "pitch": data[1], "rotation": data[2], "vel_x": data[3], "vel_z": data[4],
+                         "A": strtobool(data[5]), "B": strtobool(data[6]), "C": strtobool(data[7]),
+                         "D": strtobool(data[8]), "E": strtobool(data[9]), "F": strtobool(data[10])}
+
+        elif data[:1] == "H":
             device = "Head"
             data = data.lstrip("H")
             data = data.split(",")
             if len(data) != 4:
                 break
-            json_data = {"raw": data[0], "pitch": data[1], "vel_y": data[2], "rotation": data[3],
-                         "device": device}
+            json_data = {"raw": data[0], "pitch": data[1], "vel_y": data[2], "rotation": data[3]}
 
         path = d_path + device + ".txt"
         with open(path, "w") as f:
